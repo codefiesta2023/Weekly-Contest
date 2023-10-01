4cc13333427218bd644626ae756eb27033fe9a44
codefiesta2023 <146640127+codefiesta2023@users.noreply.github.com>
2023-10-01 22:03:02 +0530

    Update d.cpp

--- week0/cpp/d.cpp
+++ week0/cpp/d.cpp
@@ -1,3 +1,17 @@
+/*# To check if matrix is symmetric, we need to check if elements at
+# index i,j is same as element at index j,i 
+# if not increment the counter.
+
+# When in a for loop i ranges from 0 to some n and within it 
+# in another for loop j ranges from i to some m,
+# arr[i][j] traverses the upper triangular matrix 
+# and arr[j][i] traverses lower triangular matrix,
+# if we imagine the 2d array to be a matrix.
+
+# When we range j from i+1 to n, we traverse upper triangular matrix
+# without diagonal elements.In this case diagonal elements
+# do not matter so both traversal works*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
