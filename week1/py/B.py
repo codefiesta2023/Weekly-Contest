b4ff84065a894a9606e3d017cc71deb31c7da08a
codefiesta2023 <146640127+codefiesta2023@users.noreply.github.com>
2023-10-08 22:27:36 +0530

    Update B.py

--- week1/py/B.py
+++ week1/py/B.py
@@ -2,3 +2,5 @@ t=int(input())
 for _ in range(t):
     n=int(input())
     print(n-1)
+#All numbers from 1 to n-1 are co primes to a prime number n as no numbers in the
+#range 1 to n-1 are divisors of n.
